5afa5e559c8f7296127ba5d3424d9d58dfced2f5
hera <heraptt@163.com>
2025-05-09 21:07:36 +0800

    codeblock: 取消隐藏

--- queries/markdown/highlights.scm
+++ queries/markdown/highlights.scm
@@ -47,16 +47,16 @@
 ((fenced_code_block) @markup.raw.block
   (#set! priority 90))
 
-(fenced_code_block
-  (fenced_code_block_delimiter) @markup.raw.block
-  (#set! conceal "")
-  (#set! conceal_lines ""))
+; (fenced_code_block
+;   (fenced_code_block_delimiter) @markup.raw.block
+;   (#set! conceal "")
+;   (#set! conceal_lines ""))
 
-(fenced_code_block
-  (info_string
-    (language) @label
-    (#set! conceal "")
-    (#set! conceal_lines "")))
+; (fenced_code_block
+;   (info_string
+;     (language) @label
+;     (#set! conceal "")
+;     (#set! conceal_lines "")))
 
 (link_destination) @markup.link.url
 
